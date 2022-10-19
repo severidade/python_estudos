21b0a767743145180a465d35f056a06a546584da
severo <oliveira.severo@gmail.com>
2022-10-19 09:21:36 -0300

    feat: :rocket: Manipulando erros

---
+++ 06_erros.py
@@ -0,0 +1,12 @@
+try:
+    arquivo = open(file="arquivo.txt", mode="r")
+except OSError:
+    # será executado caso haja uma exceção
+    print("arquivo inexistente")
+else:
+    # será executado se tudo ocorrer bem no try
+    print("arquivo manipulado e fechado com sucesso")
+    arquivo.close()
+finally:
+    # será sempre executado, independentemente de erro
+    print("Tentativa de abrir arquivo")
